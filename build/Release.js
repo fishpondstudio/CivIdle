bd81766636da93a75c265accecb2a5edf1529240
insraq <ruoysun@gmail.com>
2023-01-28 22:30:23 +0200

    Release script
    Former-commit-id: 9db799c15acf528e0a51a5bdca6b205d2e58751b

--- build/Release.js
+++ build/Release.js
@@ -6,16 +6,27 @@ console.log("========== Building CivIdle ==========");
 
 const rootPath = path.resolve(path.join(__dirname, "../"));
 
-console.log("Root Path is", rootPath);
-
-run(`cd ${rootPath}`);
-run(`npm run build`);
+run(`npm run build`, rootPath);
 
 console.log("========== Building Electron ==========");
 
-run(`cd ${path.join(rootPath, "electron")}`);
-run(`npm run make -- --platform=win32`);
+run(`npm run package -- --platform=win32`, path.join(rootPath, "electron"));
 
-function run(command) {
-   execSync(command, { stdio: "inherit" });
+console.log("========== Uploading to Steam ==========");
+
+if (!process.env.STEAMWORKS_PATH) {
+   console.error("STEAMWORKS_PATH is not defined");
+   return;
+}
+
+fs.copySync(
+   path.join(rootPath, "electron", "out", "cividle-win32-x64"),
+   path.join(process.env.STEAMWORKS_PATH, "cividle-win32-x64")
+);
+
+run(path.join(process.env.STEAMWORKS_PATH, "cividle.sh"), process.env.STEAMWORKS_PATH);
+
+function run(command, cwd = null) {
+   console.log(`>> Command: ${command} (CWD: ${cwd})`);
+   execSync(command, { stdio: "inherit", cwd: cwd });
 }
