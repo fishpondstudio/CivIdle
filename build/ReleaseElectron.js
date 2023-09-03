d3d001ec35d14c1b751c5c6d2b801d09884b096c
insraq <ruoysun@gmail.com>
2023-09-03 18:23:57 +0300

    FIx
    Former-commit-id: 1bf03e3a6ad35ace962e5f0e5b87b384e8b584fd
    Former-commit-id: 239560426d48b0d09ef25b11d1c8c498a8eca533

--- build/ReleaseElectron.js
+++ build/ReleaseElectron.js
@@ -19,6 +19,7 @@ fs.copySync(path.join(rootPath, "dist"), path.join(rootPath, "electron", "dist")
 
 console.log("========== Build Electron ==========");
 
+cmd(`npm install`, path.join(rootPath, "electron"));
 cmd(`npm run package`, path.join(rootPath, "electron"));
 
 console.log("========== Uploading to Steam ==========");
