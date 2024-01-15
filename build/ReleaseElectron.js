ef3bde844aa4031d082269c603896655ed0a470e
insraq <ruoysun@gmail.com>
2024-01-15 19:38:47 +0200

    Update

--- build/ReleaseElectron.js
+++ build/ReleaseElectron.js
@@ -6,14 +6,26 @@ console.log("========== Building CivIdle ==========");
 
 const rootPath = path.resolve(path.join(__dirname, "../"));
 
-if (process.env.STEAMWORKS_PATH) {
+const commitMessage = execSync("git log -1").toString("utf8");
+const isRelease = commitMessage.toLowerCase().includes("[release]");
+
+if (isRelease && process.env.STEAMWORKS_PATH) {
    const versionFilePath = path.join(rootPath, "src", "scripts", "Version.json");
    const ver = JSON.parse(fs.readFileSync(versionFilePath, { encoding: "utf8" }));
    ver.build++;
    fs.writeFileSync(versionFilePath, JSON.stringify(ver));
+
+   replaceVersion(path.join(process.env.STEAMWORKS_PATH, "cividle", "CivIdle.vdf"));
+   replaceVersion(path.join(process.env.STEAMWORKS_PATH, "cividle", "CivIdle-Demo.vdf"));
+
+   function replaceVersion(path) {
+      const content = fs.readFileSync(path, { encoding: "utf8" });
+      content.replace("@Version", ver.build);
+      fs.writeFileSync(path, content);
+   }
 }
 
-cmd(`npm run build`, rootPath);
+cmd("npm run build", rootPath);
 
 console.log("========== Copy to Electron ==========");
 
@@ -21,25 +33,25 @@ fs.copySync(path.join(rootPath, "dist"), path.join(rootPath, "electron", "dist")
 
 console.log("========== Build Electron ==========");
 
-cmd(`npm run package -- --platform=win32`, path.join(rootPath, "electron"));
+cmd("npm run package -- --platform=win32", path.join(rootPath, "electron"));
 
 console.log("========== Uploading to Steam ==========");
 
 if (!process.env.STEAMWORKS_PATH) {
    console.error("STEAMWORKS_PATH is not defined");
-   return;
+   process.exit();
 }
 
 fs.removeSync(path.join(process.env.STEAMWORKS_PATH, "cividle-win32-x64"));
 
 fs.copySync(
    path.join(rootPath, "electron", "out", "cividle-win32-x64"),
-   path.join(process.env.STEAMWORKS_PATH, "cividle-win32-x64")
+   path.join(process.env.STEAMWORKS_PATH, "cividle-win32-x64"),
 );
 
 cmd(
-   path.join(process.env.STEAMWORKS_PATH, "builder_linux", "steamcmd.sh") + " +runscript ../cividle.txt",
-   process.env.STEAMWORKS_PATH
+   `${path.join(process.env.STEAMWORKS_PATH, "builder_linux", "steamcmd.sh")} +runscript ../cividle.txt`,
+   process.env.STEAMWORKS_PATH,
 );
 
 function cmd(command, cwd = null) {
