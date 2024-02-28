3008794bff965875aa9b81f62509f69b4e5cfa0e
jimmyh77 <healeyjames@yahoo.co.uk>
2024-02-28 13:03:54 +0000

    Grand Bazaar Wonder

--- shared/languages/en.ts
+++ shared/languages/en.ts
@@ -974,4 +974,9 @@ export const EN = {
    OpenSaveBackupFolder: "Open Backup Folder",
    ShortcutBuildingPageToggleBuilding: "Toggle Production",
    ShortcutBuildingPageToggleBuildingSetAllSimilar: "Toggle Production And Apply To All",
+   GrandBazaar: "Grand Bazaar",
+   GrandBazaarDesc: "Control all markets in one place!. All adjacent buildings get +5 Storage Multiplier",
+   GrandBazaarFilters: "Filters",
+   GrandBazaarFilterYouPay: "You Pay",
+   GrandBazaarFilterYouGet: "You Get",
 };
