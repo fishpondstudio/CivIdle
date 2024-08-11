09988b37d9a645f92b68b127c6971861648413b0
insraq <ruoysun@gmail.com>
2024-08-11 15:46:09 +0300

    [build]

--- shared/definitions/PatchNotes.ts
+++ shared/definitions/PatchNotes.ts
@@ -8,7 +8,7 @@ export const PatchNotes: PatchNote[] = [
    {
       version: "0.15.0",
       content: [],
-      link: "https://github.com/fishpondstudio/CivIdle/issues/304",
+      link: "https://store.steampowered.com/news/app/2181940/view/4359006996153605202",
    },
    {
       version: "0.14.0",
