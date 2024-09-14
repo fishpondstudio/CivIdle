ff9edcfa532df8c1bd3d7373f61ac3f2b59e4eef
Ruoyu Sun <ruoysun@gmail.com>
2024-09-14 16:32:20 +0300

    Patch Notes

--- shared/definitions/PatchNotes.ts
+++ shared/definitions/PatchNotes.ts
@@ -8,7 +8,7 @@ export const PatchNotes: PatchNote[] = [
    {
       version: "0.17.0",
       content: [],
-      link: "https://github.com/fishpondstudio/CivIdle/issues/323",
+      link: "https://store.steampowered.com/news/app/2181940/view/7371918503948242251",
    },
    {
       version: "0.16.0",
