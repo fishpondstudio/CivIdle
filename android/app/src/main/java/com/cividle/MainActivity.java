3500a1cc33124bdbcd60d0cd9df2925695e4137e
insraq <ruoysun@gmail.com>
2024-11-10 23:25:08 +0200

    Android

--- android/app/src/main/java/com/cividle/MainActivity.java
+++ android/app/src/main/java/com/cividle/MainActivity.java
@@ -1,13 +1,21 @@
 package com.cividle;
 
 import android.os.Bundle;
+
+import androidx.core.view.WindowCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.core.view.WindowInsetsControllerCompat;
+
 import com.getcapacitor.BridgeActivity;
 import com.openforge.capacitorgameconnect.CapacitorGameConnectPlugin;
 
 public class MainActivity extends BridgeActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        registerPlugin(CapacitorGameConnectPlugin.class);
         super.onCreate(savedInstanceState);
+        registerPlugin(CapacitorGameConnectPlugin.class);
+        var windowInsetsController = WindowCompat.getInsetsController(getWindow(), getWindow().getDecorView());
+        windowInsetsController.setSystemBarsBehavior(WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE);
+        windowInsetsController.hide(WindowInsetsCompat.Type.systemBars());
     }
 }
