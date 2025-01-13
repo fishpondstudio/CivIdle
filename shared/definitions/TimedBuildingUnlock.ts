9273d3f77bfd96adc545301b51fbb8dfc70417fb
insraq <ruoysun@gmail.com>
2025-01-13 21:41:57 +0200

    [build]

--- shared/definitions/TimedBuildingUnlock.ts
+++ shared/definitions/TimedBuildingUnlock.ts
@@ -12,7 +12,7 @@ export const TimedBuildingUnlock: Partial<Record<Building, ITimedBuildingUnlock>
       tech: "Theater",
       condition: (now) => {
          return (
-            (now.getMonth() === 0 && now.getDate() >= 10) || (now.getMonth() === 1 && now.getDate() <= 10)
+            (now.getMonth() === 0 && now.getDate() >= 20) || (now.getMonth() === 1 && now.getDate() <= 10)
          );
       },
    },
