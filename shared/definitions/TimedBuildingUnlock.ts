95aa2435d59a9cf23472ba3faeed85d8bfd6909d
insraq <ruoysun@gmail.com>
2025-04-01 17:54:26 +0300

    [build]

--- shared/definitions/TimedBuildingUnlock.ts
+++ shared/definitions/TimedBuildingUnlock.ts
@@ -18,6 +18,6 @@ export const TimedBuildingUnlock: Partial<Record<Building, ITimedBuildingUnlock>
    },
    EasterBunny: {
       tech: "PrivateOwnership",
-      condition: (now) => now.getMonth() === 2 || now.getMonth() === 3,
+      condition: (now) => now.getMonth() === 3,
    },
 };
