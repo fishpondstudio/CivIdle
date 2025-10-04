cfe8eda170c83aa120d2686414a2522ba3b0a99d
insraq <ruoysun@gmail.com>
2025-10-04 14:24:07 +0300

    [build]

--- src/scripts/ui/GameplayOptionPage.tsx
+++ src/scripts/ui/GameplayOptionPage.tsx
@@ -65,61 +65,6 @@ export function GameplayOptionPage(): React.ReactNode {
                ) : null}
             </fieldset>
             <fieldset>
-               <legend>{t(L.Tutorial)}</legend>
-               <ToggleComponent
-                  title={t(L.ShowTutorial)}
-                  contentHTML=""
-                  value={options.showTutorial}
-                  onValueChange={(value) => {
-                     playClick();
-                     options.showTutorial = value;
-                     notifyGameOptionsUpdate(options);
-                  }}
-               />
-               <div className="separator" />
-               {jsxMapOf(Todo, (id, t) => {
-                  if (id.startsWith("S")) {
-                     return null;
-                  }
-                  return (
-                     <ToggleComponent
-                        key={id}
-                        title={t.name()}
-                        contentHTML=""
-                        value={!options.disabledTodos.has(id)}
-                        onValueChange={(value) => {
-                           playClick();
-                           if (value) {
-                              options.disabledTodos.delete(id);
-                           } else {
-                              options.disabledTodos.add(id);
-                           }
-                           notifyGameOptionsUpdate(options);
-                        }}
-                     />
-                  );
-               })}
-               <div className="separator" />
-               <ToggleComponent
-                  title={t(L.ShowWonderPopup)}
-                  contentHTML=""
-                  value={options.showWonderPopup}
-                  onValueChange={(value) => {
-                     options.showWonderPopup = value;
-                     notifyGameOptionsUpdate(options);
-                  }}
-               />
-               <ToggleComponent
-                  title={t(L.ShowNaturalWonderPopup)}
-                  contentHTML=""
-                  value={options.showNaturalWonderPopup}
-                  onValueChange={(value) => {
-                     options.showNaturalWonderPopup = value;
-                     notifyGameOptionsUpdate(options);
-                  }}
-               />
-            </fieldset>
-            <fieldset>
                <legend>{t(L.GlobalBuildingDefault)}</legend>
                <div className="row mb5">
                   <div className="f1">{t(L.DefaultBuildingLevel)}</div>
@@ -267,6 +212,61 @@ export function GameplayOptionPage(): React.ReactNode {
                   onValueChange={(value) => {
                      playClick();
                      options.greedyTransport = value;
+                     notifyGameOptionsUpdate(options);
+                  }}
+               />
+            </fieldset>
+            <fieldset>
+               <legend>{t(L.Tutorial)}</legend>
+               <ToggleComponent
+                  title={t(L.ShowTutorial)}
+                  contentHTML=""
+                  value={options.showTutorial}
+                  onValueChange={(value) => {
+                     playClick();
+                     options.showTutorial = value;
+                     notifyGameOptionsUpdate(options);
+                  }}
+               />
+               <div className="separator" />
+               {jsxMapOf(Todo, (id, t) => {
+                  if (id.startsWith("S")) {
+                     return null;
+                  }
+                  return (
+                     <ToggleComponent
+                        key={id}
+                        title={t.name()}
+                        contentHTML=""
+                        value={!options.disabledTodos.has(id)}
+                        onValueChange={(value) => {
+                           playClick();
+                           if (value) {
+                              options.disabledTodos.delete(id);
+                           } else {
+                              options.disabledTodos.add(id);
+                           }
+                           notifyGameOptionsUpdate(options);
+                        }}
+                     />
+                  );
+               })}
+               <div className="separator" />
+               <ToggleComponent
+                  title={t(L.ShowWonderPopup)}
+                  contentHTML=""
+                  value={options.showWonderPopup}
+                  onValueChange={(value) => {
+                     options.showWonderPopup = value;
+                     notifyGameOptionsUpdate(options);
+                  }}
+               />
+               <ToggleComponent
+                  title={t(L.ShowNaturalWonderPopup)}
+                  contentHTML=""
+                  value={options.showNaturalWonderPopup}
+                  onValueChange={(value) => {
+                     options.showNaturalWonderPopup = value;
                      notifyGameOptionsUpdate(options);
                   }}
                />
