00df07e9d106e698288b92bd8cecb51328ec91d7
insraq <ruoysun@gmail.com>
2025-10-16 21:46:12 +0300

    [build]

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -9,6 +9,7 @@ const config: CapacitorConfig = {
    // server: {
    //    url: "http://192.168.3.12:3000/",
    // },
+   backgroundColor: "#000000",
    plugins: {
       LiveUpdate: {
          readyTimeout: 30_000,
