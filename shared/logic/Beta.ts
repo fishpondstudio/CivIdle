e29a5d2c17e3ed7bb16b045ab47606b26fd2b664
insraq <ruoysun@gmail.com>
2026-03-21 22:34:41 +0200

    Carthage

--- shared/logic/Beta.ts
+++ shared/logic/Beta.ts
@@ -4,4 +4,4 @@ import type { Material } from "../definitions/MaterialDefinitions";
 
 export const BetaBuildings = new Set<Building>([]);
 export const BetaMaterials = new Set<Material>([]);
-export const BetaCities = new Set<City>([]);
+export const BetaCities = new Set<City>(["Carthaginian"]);
