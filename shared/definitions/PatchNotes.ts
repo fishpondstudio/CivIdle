7ae93fa5693e627fe3928ee5fd6fbabce24e12d0
insraq <ruoysun@gmail.com>
2026-05-21 17:26:48 +0300

    [build]

--- shared/definitions/PatchNotes.ts
+++ shared/definitions/PatchNotes.ts
@@ -6,6 +6,11 @@ interface PatchNote {
 
 export const PatchNotes: PatchNote[] = [
    {
+      version: "1.3.0",
+      content: [],
+      link: "https://store.steampowered.com/news/app/2181940/view/680750978144665918",
+   },
+   {
       version: "1.2.0",
       content: [],
       link: "https://store.steampowered.com/news/app/2181940/view/512987452523153485",
